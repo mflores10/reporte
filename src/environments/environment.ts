09a0870c4a3b1541d071d3ca2020aa8b4624ff0a
mflores10 <itsg.mflores1@gmail.com>
2019-05-12 19:19:06 -0600

    nav

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    projectId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    apiKey: "AIzaSyD-Y7ljzb4IJ9bf5P1sIb7ZKOeRV1V-Zmw",
+    authDomain: "forms-3ed4b.firebaseapp.com",
+    databaseURL: "https://forms-3ed4b.firebaseio.com",
+    projectId: "forms-3ed4b",
+    storageBucket: "forms-3ed4b.appspot.com",
+    messagingSenderId: "659270758229",
+    appId: "1:659270758229:web:28f36cfa189292ac"
   }
 };
 
