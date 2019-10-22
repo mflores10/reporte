23054272ca7a80dbebf45cd52938b049aee34fa9
mflores10 <itsg.mflores1@gmail.com>
2019-10-22 11:27:03 -0600

    Terminado

--- src/app/pipe/pipe-date.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PipeDatePipe } from './pipe-date.pipe';
-
-describe('PipeDatePipe', () => {
-  it('create an instance', () => {
-    const pipe = new PipeDatePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
